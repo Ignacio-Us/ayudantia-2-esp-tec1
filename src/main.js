503887fc4e669a48b10181ee55639100a198252f
Ignacio-Us <i.uribe01@ufromail.cl>
2024-05-17 14:57:31 -0400

    Agregacion de dependencias y router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { router } from "./router/router.js";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css';
+
+const app =createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ src/router/router.js
@@ -0,0 +1,10 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+
+];
+
+export const router  = createRouter({
+    history: createWebHistory(),
+    routes
+});
